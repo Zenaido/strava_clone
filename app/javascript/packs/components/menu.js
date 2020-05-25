de9b252702919b4825f12b5d25c165f15f277278
zenaido <luis.hernandez@aggiemail.usu.edu>
2020-05-25 16:39:23 -0600

    Basic navbar with dropdown

--- app/javascript/packs/components/menu.js
+++ app/javascript/packs/components/menu.js
@@ -1,17 +1,17 @@
-import React from "react";
-import MenuList from "@material-ui/core/Menu";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import Grow from "@material-ui/core/Grow";
-import Paper from "@material-ui/core/Paper";
-import Popper from "@material-ui/core/Popper";
-import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
-import { makeStyles } from "@material-ui/core/styles";
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import {makeStyles} from '@material-ui/core/styles';
+import React from 'react';
 
 const Menu = (props) => {
-  const { open, anchorEl } = props;
+  const { open, anchorEl, handleClose, children } = props;
+  const l = React.Children.map(children, (child) => { return <MenuItem key={child} onClick={handleClose}>{child}</MenuItem>});
   return (
-    <Popper keepMounted open={open} anchorEl={anchorEl}>
+    <Popper open={open} anchorEl={anchorEl} role={undefined} transition disablePortal>
       {({ TransitionProps, placement }) => (
             <Grow
               {...TransitionProps}
@@ -19,15 +19,14 @@ const Menu = (props) => {
             >
               <Paper>
                 <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
-                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  <MenuList autoFocusItem={open} id='menu-list-grow'  >
+                    {l}
                   </MenuList>
                 </ClickAwayListener>
               </Paper>
-            </Grow>
-   
+            </Grow>)}
     </Popper>
   );
 };
+
+export default Menu;
--- app/javascript/packs/components/navigation_bar.js
+++ app/javascript/packs/components/navigation_bar.js
@@ -1,34 +1,96 @@
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
-import axios from 'axios'
-import React from "react";
 import { makeStyles } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import Toolbar from "@material-ui/core/Toolbar";
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import axios from "axios";
+import React, { Fragment } from "react";
+
+import Menu from "./menu";
+
+// const token = getToken();
+// axios.post(
+//   "/users/sign_in",
+//   {
+//     user: {
+//       email: "lzhernandez90@gmail.com",
+//       password: "Tackjkjk23obell4379!",
+//       password_confirmation: "Tackjkjk23obell4379!",
+//     },
+//   },
+//   {
+//     headers: {
+//       "X-CSRF-Token": token,
+//     },
+//   }
+// );
+
+const useStyles = makeStyles({
+  leftSide: { flex: 80 },
+  rightSide: { flex: 4 },
+});
 
 const getToken = () =>
-  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-const NavigationBar = (props) => {
-  return (
-    <AppBar position="static">
-      <Toolbar placement="bottom">
-        <div>Test</div>
-        <IconButton
-          onClick={async () => {
-            const token = getToken();
-            axios.post('/users/sign_in',{user: {email: 'lzhernandez90@gmail.com', password:'Tackjkjk23obell4379!', password_confirmation: 'Tackjkjk23obell4379!'}}, {
-              headers: {
-                'X-CSRF-Token': token,
-              }
-            });
+  document.querySelector('meta[name="csrf-token"]').getAttribute("content");
 
-            console.log("howdy");
-          }}
-        >
-          <AccountCircleRoundedIcon htmlColor="white" />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+const NavigationBar = (props) => {
+  const classes = useStyles();
+  const anchorRef = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+  const handleToggle = () => {
+    console.log("howdy 2");
+    setOpen((prevOpen) => !prevOpen);
+  };
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+  console.log(open);
+  return (
+    <Fragment>
+      <AppBar position="static">
+        <Toolbar placement="bottom">
+          <div className={classes.leftSide}>Test</div>
+          <div className={classes.rightSide}>
+            <IconButton
+              ref={anchorRef}
+              onClick={() => {
+                handleToggle();
+              }}
+            >
+              <AccountCircleRoundedIcon htmlColor="white" />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Menu
+        anchorEl={anchorRef.current}
+        open={Boolean(open)}
+        handleClose={handleClose}
+      >
+        <Fragment>Profile</Fragment>
+        <Fragment>My account</Fragment>
+        <Fragment>Logout</Fragment>
+      </Menu>
+    </Fragment>
   );
 };
 
